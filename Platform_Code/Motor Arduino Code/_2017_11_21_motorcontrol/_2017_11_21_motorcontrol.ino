24f5b4f42d28aab3446c3ebdab05b032146fe4d7
Alexander Brown <brownaa@lafayette.edu>
2022-02-01 10:55:40 -0500

    first commit

---
+++ Platform_Code/Motor Arduino Code/_2017_11_21_motorcontrol/_2017_11_21_motorcontrol.ino
@@ -0,0 +1,329 @@
+
+// Assign your channel in pins
+#define CHANNEL_A_PIN 2
+#define CHANNEL_B_PIN 3
+volatile long unCountShared;
+long unCount;
+float unCount_float;
+float initfloat;
+
+//A0 reads in angle
+//0-5V = 0-pi
+const int analogInPin=A0;
+const int analogOutPin1=11;
+const int analogOutPin2=9;
+const int analogOutPin3=10;
+const int jogmodePin = 12;
+const int openloopPin = 13;
+
+float k_pot = (270.0)/(PI/2);
+
+
+int val = 0; 
+int val2 = 0;
+int mag = 0;
+
+
+//these are the zeros for each motor....
+//static int globzeros[6] = {285,840,275,730,275,770};
+//static int globzeros[6] = {525,540,500,500,535,500};
+static int globzeros[6] = {500,850,450,500,500,500};
+
+
+static float motorsigns[6] = {1.0,-1.0,1.0,-1.0,1.0,-1.0};
+static int zero[6]={1470,1470,1470,1470,1470,1470}; //Zero positions of servos
+
+// ONLY USE ARDUINO 2:1.0.5 WITH THIS CODE DO NOT USE OLD VERSION
+
+//put the motor number for this arduino here:
+int motornum = 5;
+
+
+
+float deadBand = .031;
+
+int glob0 = globzeros[motornum-1];
+
+# define PWM_SOURCE 7
+float servo_mult=1000.0/(PI);
+
+float ref_command_float = 0;
+int rawpos;
+float posfloat = 0;
+
+boolean jogmode=true;
+boolean openloop = false;
+
+void setup()
+{
+//  if(motornum == 3)
+//{
+//float deadBand = .6;
+//}
+  pinMode(analogOutPin2,INPUT);
+  pinMode(analogOutPin3,INPUT);
+  pinMode(analogOutPin1,INPUT);
+  Serial.begin(115200);
+  //attach the interrupts
+  attachInterrupt(0, channelA,CHANGE);
+  attachInterrupt(1, channelB,CHANGE);
+//  int tot = 0;
+//  for (int k=0; k<100; k++){
+//    tot += analogRead(1);
+//    delay(1);
+//  }
+//  int globV = tot/100;
+//  int globV = analogRead(1);
+//  Serial.print(globV);
+//  Serial.print('\t');
+//  int glob0 = 100;
+//  //float k_pot = (390.0-100.0)/(PI/4);
+//  float globPos = (globV - glob0)/k_pot;
+//  Serial.print(globPos);
+//  Serial.print('\t');
+//  //unCountShared = long( globPos*8000/(2*PI) );
+//  //Serial.print(unCountShared);
+//  Serial.print('\t');
+//  delay(1000);
+float initsum = 0;
+delay(10);
+for (int i = 0; i<200; i++)
+{
+  initsum = initsum + analogRead(1);
+}
+
+rawpos = initsum/200;
+initfloat = motorsigns[motornum-1]*(rawpos - glob0)/k_pot;
+
+pinMode(analogOutPin2,OUTPUT);
+pinMode(analogOutPin3,OUTPUT);
+pinMode(analogOutPin1,OUTPUT);
+}
+
+void loop()
+{
+  // create local variables to hold a local copies of the channel inputs
+  // these are declared static so that thier values will be retained
+  // between calls to loop.
+  //static long unCount;
+        	
+  //noInterrupts(); // turn interrupts off quickly while we take local copies of the shared variables
+  jogmode=digitalRead(jogmodePin);
+  openloop = digitalRead(openloopPin);
+  //unCount = unCountShared;
+  if (jogmode==false){
+   // static long unCount;
+        	
+    noInterrupts(); // turn interrupts off quickly while we take local copies of the shared variables
+
+    unCount = unCountShared;
+ 
+    interrupts(); 
+    
+    unCount_float = motorsigns[motornum-1]*unCount*2*PI/8000.0 + initfloat;
+  
+    //float posfloat = unCount*2*PI/(8000.0); //our encoder position in radians
+    rawpos = analogRead(1);
+    posfloat = motorsigns[motornum-1]*(rawpos - glob0)/k_pot;
+    //val is desired angle
+    //0-1023 = 0-pi
+    //val = analogRead(analogInPin); //raw reference position value.
+    //val = val-512; //this number should be scaled to go from -PI to PI
+    val = pulseIn(PWM_SOURCE, HIGH, 20000);
+    //float ref_command_float = val*PI/512.0;//this should be the reference position in radians.
+    if (millis()>5000)
+    {
+      ref_command_float = (val - zero[motornum-1])/servo_mult; //this should give the reference position, converted from microseconds to radians
+    }
+  }
+  
+  else{
+    //interrupts(); 
+    noInterrupts(); // turn interrupts off quickly while we take local copies of the shared variables
+
+    unCount = unCountShared;
+ 
+    interrupts(); 
+    
+    
+    
+    unCount_float = motorsigns[motornum-1]*unCount*2*PI/8000.0 + initfloat;
+    rawpos = analogRead(1);
+    posfloat = motorsigns[motornum-1]*(rawpos - glob0)/k_pot;
+    
+
+    val = analogRead(analogInPin); //raw reference position value.
+    val=val-512; //this number should be scaled to go from -PI to PI
+  
+    ref_command_float = val*PI/512.0;//this should be the reference position in radians.
+      
+}
+  
+  if (ref_command_float>1.0){
+    ref_command_float = 1.0;
+  }
+  if (ref_command_float<-1.0){
+    ref_command_float = -1.0;
+  }
+  
+  float float_error = ref_command_float-unCount_float;//this is our current error value!!
+  //float encoder_error = ref_command_float-unCount_float;
+  
+  float kp = 255.0/(PI/6.0);//some guess for KP.........
+  float float_U = 0;
+  if(openloop==false){
+  float_U = kp*float_error;
+  }
+  else{
+  float_U = (analogRead(analogInPin)-512)*255.0/512.0; 
+  }
+  
+  if(float_U>255.0){
+    float_U = 255.0;
+  }
+  
+  if (float_U<-255.0){
+    float_U = -255.0;
+  }
+  
+  val2 = int(float_U);
+  mag = abs(val2);
+  mag = constrain(mag,0,128);
+
+if(openloop==false){
+  if(float_error>deadBand){
+    //Serial.print("!!!!!!!!!!!!");
+   digitalWrite(analogOutPin3,HIGH);
+   analogWrite(analogOutPin1,mag);
+   digitalWrite(analogOutPin2,LOW);
+ }
+ else if(float_error<-deadBand){
+   digitalWrite(analogOutPin2,HIGH);
+   analogWrite(analogOutPin1,mag);
+   digitalWrite(analogOutPin3,LOW);
+ }
+ 
+ else{
+   digitalWrite(analogOutPin3,LOW);
+   analogWrite(analogOutPin1,0);
+   digitalWrite(analogOutPin2,LOW);
+ }
+}
+else{
+  if(float_U>deadBand){
+    //Serial.print("!!!!!!!!!!!!");
+   digitalWrite(analogOutPin3,HIGH);
+   analogWrite(analogOutPin1,mag);
+   digitalWrite(analogOutPin2,LOW);
+ }
+ else if(float_U<-deadBand){
+   digitalWrite(analogOutPin2,HIGH);
+   analogWrite(analogOutPin1,mag);
+   digitalWrite(analogOutPin3,LOW);
+ }
+ 
+ else{
+   digitalWrite(analogOutPin3,LOW);
+   analogWrite(analogOutPin1,0);
+   digitalWrite(analogOutPin2,LOW);
+ }
+}
+ 
+//  Serial.print(posfloat);
+//  Serial.print("\t");
+//  Serial.print(unCount_float);
+//  Serial.print("\t");
+//  Serial.print(float_error);
+//  Serial.print("\t"); 
+//  Serial.print(unCount);
+//  Serial.print("\t");
+
+//raw reference position value 0-1023 (open or closed loop)
+  Serial.print(val);
+  Serial.print("\t");
+  //raw potentiometer position
+  Serial.print(rawpos);
+  Serial.print("\t");
+  //position estimate in radians based on potentiometer
+    Serial.print(posfloat);
+  Serial.print("\t");
+  //position estimate based on encoder (should match above)
+      Serial.print(unCount_float);
+  Serial.print("\t");
+  //magnitude of motor command
+  Serial.print(mag);
+//  Serial.print("\t");
+//  Serial.print(val2);
+
+  Serial.println();
+
+//Serial.print(ref_command_float*180/PI);
+//Serial.print("\t");
+//Serial.print(posfloat*180/PI);
+//Serial.print("\t");
+//Serial.println(unCount_float*180/PI);
+  
+//  Serial.println(val);
+//  Serial.println(mag);
+//  Serial.print(millis());
+//  Serial.print("\t");
+//  Serial.println(unCount);
+  delay(1);
+  
+}
+
+
+// simple interrupt service routine
+void channelA()
+{
+  //Serial.println("A");
+  if (digitalRead(CHANNEL_A_PIN) == HIGH)
+  {
+	if (digitalRead(CHANNEL_B_PIN) == LOW)
+	{
+  	unCountShared++;
+	}
+	else
+	{
+  	unCountShared--;
+	}
+  }
+  else
+  {
+	if (digitalRead(CHANNEL_B_PIN) == HIGH)
+	{
+  	unCountShared++;
+	}
+	else
+	{
+  	unCountShared--;
+	}
+  }
+}
+
+void channelB()
+{
+  //Serial.println("B");
+  if (digitalRead(CHANNEL_B_PIN) == HIGH)
+  {
+	if (digitalRead(CHANNEL_A_PIN) == HIGH)
+	{
+  	unCountShared++;
+	}
+	else
+	{
+  	unCountShared--;
+	}
+  }
+  else
+  {
+	if (digitalRead(CHANNEL_A_PIN) == LOW)
+	{
+  	unCountShared++;
+	}
+	else
+	{
+  	unCountShared--;
+	}
+  }
+}
---
+++ interface/platform_control.py
@@ -0,0 +1,8 @@
+from TKinter import *
+
+master = TK()
+
+master.title('Lafayette Motion Platform Control')
+
+xslider = Scale(master,from_=0,to=100)
+
