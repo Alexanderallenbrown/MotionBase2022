81437a4f469feb8d8322ddca024daaed7367e975
Alexander Brown <brownaa@lafayette.edu>
2022-02-01 16:04:37 -0500

    progress on gui

--- interface/platform_control.py
+++ interface/platform_control.py
@@ -13,6 +13,8 @@ import Tkinter as tk
 #create a global variable to hold the platform command
 cmd = [0,0,0,0,0,0]
 # command = [xdes_final,ydes_final,zdes_final,rdes_final,pdes_final,ades_final]
+endSerialThread = False
+
 
 ############### CALLBACKS FOR SLIDERS ###################
 def rollcallback(v):
@@ -26,29 +28,37 @@ def xcallback(v):
 def ycallback(v):
     cmd[1]=yslider.get()
 def zcallback(v):
-    cmd[2]=zslider.get()+4
+    cmd[2]=zslider.get()+5
 
 ############## CALLBACK FOR RUN BUTTON ##################
 
 def startPlatformThread():
+    global platthread,endSerialThread
+    endSerialThread = False
+    statemsg["text"] = "Connected"
     platthread = Thread(target=doPlatform)
     platthread.start()
+    
+def cleanupPlatformThread():
+    global endSerialThread
+    endSerialThread=True
+    
 
 
 
 ############## Function to communicate with Arduino ###########
 def doPlatform():
-    global cmd
+    global cmd,ser,endSerialThread
     #initialize old time
-    lastsendtime = time.time()
-    arduino_delay = 0.1
+    arduino_delay = 0.01
 
     #connect to the serial port.
     #the portentry.get() call gets the port name
     #from the textbox.
     ser = serial.Serial(
     port=portentry.get(), #ACM100',   #USB0', 
-    baudrate=115200) 
+    baudrate=115200,
+    timeout = .1) 
     print("initializing")
     ser.close()
     time.sleep(2.0)
@@ -56,26 +66,36 @@ def doPlatform():
     time.sleep(2.0)
     print("done") 
     
-    #this is an infinite loop.
-    while 1:
+    starttime = time.time()
+    lastsendtime = time.time()-starttime
+    #this is an infinite loop  .
+    while not endSerialThread:
+
         #get current time
-        tnow = time.time()
+        tnow = time.time()-starttime
+        # print(tnow-lastsendtime)
         if tnow-lastsendtime>arduino_delay:     ### also happens super fast
             #print 'sent'
             print("sent: "+format(cmd[0],'0.2f')+","+format(cmd[1],'0.2f')+","+format(cmd[2],'0.2f')+","+format(cmd[3],'0.4f')+","+format(cmd[4],'0.4f')+","+format(cmd[5],'0.4f'))
             lastsendtime = tnow
             ser.write('!')
             for ind in range(0,len(cmd)-1):
-              ser.write(format(cmd[ind],'0.2f'))
+              ser.write(format(cmd[ind],'0.3f'))
               ser.write(',')
             ser.write(str(cmd[-1]))
             ser.write('\n')
-            line = ser.readline()
-            print ("recieved: "+line)
+            # time.sleep(0.01)
+            #line = ser.readline()
+            #print ("at time = "+format(tnow,'0.2f')+ " received: "+line)
+            #print(line)
+            #echomsg["text"] = "at t = "+format(tnow,'0.2f')+" received: "+line
         else:
-          time.sleep(.01)
+          time.sleep(.1)
         # print(cmd)
         # time.sleep(0.1)
+    ser.close()
+    statemsg["text"] = "Not Connected"
+
 
 
 
@@ -99,13 +119,18 @@ portmsg.pack(side=tk.LEFT)
 portentry = tk.Entry(portframe)
 #insert a default port
 portentry.insert(0,"/dev/ttyUSB100")
-portentry.pack(side=tk.RIGHT)
+portentry.pack(side=tk.LEFT)
 # create a button to connect to platform
 platbut = tk.Button(
     portframe,
     text="Connect",
     command=startPlatformThread)
 platbut.pack(side=tk.RIGHT)
+killbut = tk.Button(
+    portframe,
+    text="Disconnect",
+    command=cleanupPlatformThread)
+killbut.pack(side=tk.RIGHT)
 #create a status message in this frame to show serial port status
 statemsg = tk.Label(window,text="Not Connected")
 statemsg.pack()
@@ -140,7 +165,7 @@ xframe = tk.Frame(window,relief=tk.GROOVE,borderwidth=3)
 xframe.pack()
 xlabel = tk.Label(xframe,text="x (rad)")
 xlabel.pack(side=tk.LEFT)
-xslider = tk.Scale(xframe,orient=tk.HORIZONTAL,from_=-0.1,to=0.1,resolution=.001,command=xcallback)
+xslider = tk.Scale(xframe,orient=tk.HORIZONTAL,from_=-2,to=2,resolution=.1,command=xcallback)
 xslider.pack(side=tk.RIGHT)
 
 #create a slider for y
@@ -148,7 +173,7 @@ yframe = tk.Frame(window,relief=tk.GROOVE,borderwidth=3)
 yframe.pack()
 ylabel = tk.Label(yframe,text="y (rad)")
 ylabel.pack(side=tk.LEFT)
-yslider = tk.Scale(yframe,orient=tk.HORIZONTAL,from_=-0.1,to=0.1,resolution=.001,command=ycallback)
+yslider = tk.Scale(yframe,orient=tk.HORIZONTAL,from_=-2,to=2,resolution=.1,command=ycallback)
 yslider.pack(side=tk.RIGHT)
 
 #create a slider for z
@@ -156,10 +181,12 @@ zframe = tk.Frame(window,relief=tk.GROOVE,borderwidth=3)
 zframe.pack()
 zlabel = tk.Label(zframe,text="z (rad)")
 zlabel.pack(side=tk.LEFT)
-zslider = tk.Scale(zframe,orient=tk.HORIZONTAL,from_=-0.1,to=0.1,resolution=.001,command=zcallback)
+zslider = tk.Scale(zframe,orient=tk.HORIZONTAL,from_=-2,to=2,resolution=.1,command=zcallback)
 zslider.pack(side=tk.RIGHT)
 
-
+#create a status message in this frame to show serial port status
+echomsg = tk.Label(window,text="No Data Received")
+echomsg.pack()
 
 
 
