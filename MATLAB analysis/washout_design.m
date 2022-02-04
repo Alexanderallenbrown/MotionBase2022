9ee7283fb515693cc7ad00da7fdf5a13480d7b77
Alexander Brown <brownaa@lafayette.edu>
2022-02-04 17:45:19 -0500

    work on washout library

--- MATLAB analysis/washout_design.m
+++ MATLAB analysis/washout_design.m
@@ -15,7 +15,7 @@ s = tf('s');
 scale = 0.25;
 
 z = 1;
-wn = 10;
+wn = 1;
 t = 0:.001:4;
 %high pass filter for x, y
 Ghp = s^2/(s^2+2*z*wn*s+wn^2);
@@ -73,7 +73,7 @@ title('1g acceleration step response in y')
 %% TILT FILTER (low pass)
 
 %now simulate the low-pass filter used to develop tilt command
-wlp = 100;
+wlp = 10;
 zlp = 1;
 Glp = wlp^2/(s^2+2*zlp*wlp*s+wlp^2);
 [aylp,t]=step(Glp*scale,t);
--- interface/washout.py
+++ interface/washout.py
@@ -21,265 +21,155 @@ class Washout:
     #call the function to do the filter definitions.
     #may want to update this function to make filters not hard-coded...
     #possibly pass them in as options
-    self.updateFilters(dt)
+    self.updateFilterCoefficients(dt)
 
     #prepare vectors of "past" values for filters to use
     # we have a max of a 4th order TF, so we will include 4 "lagged" values
+    self.xvec = [0,0,0,0,0] #x position (m) comes from HIGH PASS scoot filter
+    self.yvec = [0,0,0,0,0] #y position (m) comes from HIGH PASS scoot filter
+    self.zvec = [0,0,0,0,0] #z position (m) comes from HIGH PASS scoot filter
     self.axvec = [0,0,0,0,0] #x acceleration (m/s/s)
     self.ayvec = [0,0,0,0,0] #y acceleration (m/s/s)
     self.azvec = [0,0,0,0,0] #z acceleration (m/s/s)
-    self.wzvec = [0,0,0,0,0] #yaw rate (rad/s)
+    self.avec = [0,0,0,0,0] #yaw ANGLE (rad) comes from high pass "scoot" filter
+    self.axrawvec = [0,0,0,0,0] #raw (input) x acceleration (m/s/s)
+    self.ayrawvec = [0,0,0,0,0] #raw (input) y acceleration (m/s/s)
+    self.azrawvec = [0,0,0,0,0] #raw (input) z acceleration (m/s/s)
+    self.wzrawvec = [0,0,0,0,0] #raw (input) yaw rate (rad/s)
     self.g = 9.81
 
-  def updateFilters(self,dt):
+  def updateFilterCoefficients(self,dt):
     #Washout filter definitions (could read from a file or make adjustable...)
     
     #filter 1: high pass that produces a y command from y acceleration ("scoot" filter)
-    # G(s) = 10s^2/(s^2+10s+20)
-    (self.numyhp,self.denyhp,self.dtyhp) = cont2discrete(([10,0],[1,10,20]),self.dt) #numerator and denominator coefficients
+    # G(s) =                      s
+  #           ----------------------------------
+  #           s^3 + 2.333 s^2 + 1.667 s + 0.3333
+
+    (self.numyhp,self.denyhp,self.dtyhp) = cont2discrete(([1,0],[1,2.333,1.667,.333]),self.dt) #numerator and denominator coefficients
 
     #filter 2: low pass that produces a roll angle to simulate sustained acceleration in y
     #ay to roll
-    (self.numylp,self.denylp,self.dtylp) = cont2discrete(([-32500.],[1.,100.,1300.]),self.dt)
+    (self.numylp,self.denylp,self.dtylp) = cont2discrete(([100.],[1.,20.,100.]),self.dt)
 
     #filter 3: high pass that produces a x command from x acceleration ("scoot" filter)
     # G(s) = 10s^2/(s^2+10s+20)
-    (self.numxhp,self.denxhp,self.dtxhp) = cont2discrete(([10,0],[1,10,20]),self.dt) #numerator and denominator coefficients
+    (self.numxhp,self.denxhp,self.dtxhp) = cont2discrete(([1,0],[1,2.333,1.667,.333]),self.dt) #numerator and denominator coefficients
 
     #filter 4: low pass that produces a pitch angle to simulate sustained acceleration in x
     #ax to pitch
-    (self.numxlp,self.denxlp,self.dtxlp) = cont2discrete(([-32500.],[1.,100.,1300.]),self.dt)
+    (self.numxlp,self.denxlp,self.dtxlp) = cont2discrete(([100.],[1.,20.,100.]),self.dt)
 
 
     #filter 5: high pass filter producing a z command from z acceleration (scoot)
     #az to z_desired
-    (self.numzhp,self.denzhp,self.dtzhp) = cont2discrete(([10.,0],[1.,11.,110.,100.]),dt[-1])
+    (self.numzhp,self.denzhp,self.dtzhp) = cont2discrete(([1,0],[1,2.333,1.667,.333]),dt[-1])
 
     #filter 6: high pass filter producing a anglez to anglez_filtered
     # instead of "y" for yaw we will use "a" for "azimuth"
-    (self.numahp,self.denahp,self.dtahp) = cont2discrete(([1,0,0],[1.,2.,4.]),dt[-1])
+    (self.numahp,self.denahp,self.dtahp) = cont2discrete(([1,0],[1,2.333,1.667,.333]),dt[-1])
 
 
   #this function gets called over and over in the loop. It takes in accelerations and yaw rate
   #it produces and updated guess for the simulated accelerations and yaw rates.
   def doWashout(self,ax_raw,ay_raw,az_raw,wz_raw):
+    # MAKE SURE THESE INPUT ANGLES ARE IN G for accelerations and rad/s for yaw rate!!
+
     #first we need to scale the signals down.
     ax_raw = ax_raw*self.axscale
     ay_raw = ay_raw*self.ayscale
     az_raw = az_raw*self.azscale
     wz_raw = wz*self.wzscale
 
+
     #now we actually apply the filters. the "raw" accel is the input to each TF
     #the vectors ax,ay, etc. are the "output."
+    #y_desired[3]=-den1[1]*y_desired[2]-den1[2]*y_desired[1]+num1[1]*ay_raw_small[2]+num1[2]*ay_raw_small[1]
+    
+    # all high pass filters below
+    xfilt = 1.0/self.denxhp[0]*(-self.denxhp[1]*self.xvec[0] - self.denxhp[2]*self.xvec[-1] - self.denxhp[3]*self.xvec[-2] + ax_raw*self.numxhp[0] + self.axrawvec[-1]*self.numxhp[-1] + self.axrawvec[-2]*self.numxhp[-2])
+    yfilt = 1.0/self.denyhp[0]*(-self.denyhp[1]*self.yvec[0] - self.denyhp[2]*self.yvec[-1] - self.denyhp[3]*self.yvec[-2] + ax_raw*self.numyhp[0] + self.ayrawvec[-1]*self.numyhp[-1] + self.ayrawvec[-2]*self.numyhp[-2])
+    zfilt = 1.0/self.denzhp[0]*(-self.denzhp[1]*self.zvec[0] - self.denzhp[2]*self.zvec[-1] - self.denzhp[3]*self.zvec[-2] + ay_raw*self.numzhp[0] + self.azrawvec[-1]*self.numzhp[-1] + self.azrawvec[-2]*self.numzhp[-2])
+    afilt = 1.0/self.denahp[0]*(-self.denahp[1]*self.avec[0] - self.denahp[2]*self.avec[-1] - self.denahp[3]*self.avec[-2] + wz_raw*self.numahp[0] + self.wzrawvec[-1]*self.numahp[-1] + self.wzrawvec[-2]*self.numahp[-2])
 
-    #now we have to update our "buffers" of past acceleration values
+    #all low pass filters below
+    axfilt = 1.0/self.denxlp[0]*(-self.denxlhp[1]*self.axvec[0] - self.denxlp[2]*self.axvec[-1] + ax_raw*self.numxlp[0] + self.axrawvec[-1]*self.numxlp[-1])
+    ayfilt = 1.0/self.denylp[0]*(-self.denylhp[1]*self.ayvec[0] - self.denylp[2]*self.ayvec[-1] + ay_raw*self.numylp[0] + self.ayrawvec[-1]*self.numylp[-1])
+
+    #now we can compute the tilt angles in RADIANS.
+    #positive roll is to driver's right side, so to feel like accelerating in positive y (drive left), need to take neg
+    roll = -arcsin(ayfilt)
+    #positive pitch is down towards driver feet, so to feel like accelerating in positive x, need to take neg
+    pitch = -arcsin(axfilt)
+
+    #now we have to update our "buffers" of past acceleration and position values
+    #pop(0) removes the first element in the array. append() adds a new value to the end.
+    #the next lines remove the "oldest" value in each vector and replace the last element with the newest.
+    self.xvec.pop(0);self.xvec.append(xfilt)
+    self.yvec.pop(0);self.yvec.append(yfilt)
+    self.zvec.pop(0);self.zvec.append(zfilt)
+    self.axvec.pop(0);self.axvec.append(axfilt)
+    self.ayvec.pop(0);self.ayvec.append(ayfilt)
+    self.azvec.pop(0);self.azvec.append(azfilt)
+    self.avec.pop(0);self.avec.append(afilt)
+
+    #now we put raw input values in their "lagged" arrays so we can use "old" values in the filter as required.
     #pop(0) removes the first element in the array. append() adds a new value to the end.
     #the next 3 lines remove the "oldest" value of acceleration and replace the last element with the newest.
-    self.ax.pop(0);self.ax.append(axfilt)
-    self.ay.pop(0);self.ay.append(ayfilt)
-    self.az.pop(0);self.az.append(azfilt)
-    self.wz.pop(0);self.wz.append(wzfilt)
-
-    
+    self.axrawvec.pop(0);self.axrawvec.append(axraw)
+    self.ayrawvec.pop(0);self.ayrawvec.append(ayraw)
+    self.azrawvec.pop(0);self.azrawvec.append(azraw)
+    self.wzrawvec.pop(0);self.wzrawvec.append(wzraw)
 
 
+    #now return the commands to the platform. each call to this function returns the scalar value that's relevant NOW
+    return xfilt,yfilt,zfilt,roll,pitch,afilt
 
 
-###### Implements Washout Filters (design these using MATLAB)
-def doWashout(ax_raw,ay_raw,az_raw,wx_raw,wy_raw,wz_raw,dt):
-
-  global platform_port, t, ax_raw, ay_raw, az_raw, anglex_raw, angley_raw, anglez_raw, buffsize, anglezraw#, dt
-
-  # initialize position variables
-  N = 4
-  x_desired = zeros(N)
-  y_desired = zeros(N)
-  z_desired = zeros(N)
-  ax_tilt = zeros(N)
-  ay_tilt = zeros(N)
-  ax_raw_small = zeros(N)
-  ay_raw_small = zeros(N)
-  az_raw_small = zeros(N)
-  anglex_raw_small = zeros(N)
-  angley_raw_small = zeros(N)
-  anglez_raw_small = zeros(N)
-  anglex = 0
-  angley = 0
-  anglex_filtered=zeros(N)
-  angley_filtered=zeros(N)
-  anglez_filtered=zeros(N)
-  anglez=zeros(N)
-  ax_tiltLP=0
-  ay_tiltLP=0
-  index=0 
-  command = [0,0,0,0,0,0]
-
-  # initialize time variables
-  starttime = time.time()
-  oldtime = time.time()
-  time.sleep(.01)
-  # dt = 0.005
-  lastsendtime = time.time()
-  arduino_delay = 0.01
-  lastfilttime = time.time()
-  filter_delay = 0.05
-
-  platform_port = startSerial()
-   
-  print len(t)
-  while 1:        ## >>> 500 Hz
-    
-    tnow = time.time()  # what time is it mr. fox??
-    #print tnow
-    #print len(t)
-    if len(t)>=buffsize:
-      # timetime = time.time()
-      # take most recent value read from buffer
-      # trecent = time.time()
-      ax_raw_small = append(ax_raw_small[1:],ax_raw[-1])
-      ay_raw_small = append(ay_raw_small[1:],ax_raw[-1])
-      az_raw_small = append(az_raw_small[1:],ax_raw[-1])
-      anglex_raw_small = append(anglex_raw_small[1:],anglex_raw[-1])
-      angley_raw_small = append(angley_raw_small[1:],angley_raw[-1])
-      anglez_raw_small = append(anglez_raw_small[1:],anglez_raw[-1])
-      
-      # tapp = tnow-trecent
-      # print tapp
-      #print tnow-lastfilttime
-      if (tnow-lastfilttime)>filter_delay:              ## filters run at ~500 Hz
-        
-        # tstartfilt = time.time()
-        #determine time step
-        # print dt[-1]
-        dt = array([tnow-lastfilttime])
-        #print dt
-        lastfilttime = tnow #have to reset this!! otherwise we don't keep track of this properly.
-        
-        # oldtime = tnow
-
-        scaledown_roll = 0.0 #or .6
-        scaledown_pitch = 2.0
-
-        #ay to y_desired, and ax to x_desired
-        (num1,den1,dt1) = cont2discrete(([10,0],[1,10,20]),dt[-1])
-
-        #ay to ax_tilt
-        (num2,den2,dt2) = cont2discrete(([-32500.*scaledown_roll],[1.,100.,1300.]),dt[-1])
-
-        #copy of that for x only
-        (num2a,den2a,dt2) = cont2discrete(([-32500.*scaledown_pitch],[1.,100.,1300.]),dt[-1])
-  
-        #anglex to anglex_filtered
-        (num3,den3,dt3) = cont2discrete(([1,0],[1.,2.,4.]),dt[-1])
-
-        #az to z_desired
-        (num4,den4,dt4) = cont2discrete(([10.,0],[1.,11.,110.,100.]),dt[-1])
-
-        #anglez to anglez_filtered
-        (num5,den5,dt5) = cont2discrete(([1,0,0],[1.,2.,4.]),dt[-1])
-
-        num1,num2,num2a,num3,num4,num5 = num1[0],num2[0],num2a[0],num3[0],num4[0],num5[0]
-
-        x_desired = append(x_desired[1:],0)
-        y_desired = append(y_desired[1:],0)
-        z_desired = append(z_desired[1:],0)+2.0#####TODO!!!! THIS IS A TEMP
-
-        anglez_filtered = append(anglez_filtered[1:],0)
-
-        # timeendfilt = time.time()
-        # filtTime=timeendfilt-tstartfilt
-        # print filtTime
-        # timetime = time.time()
-        #final values: x_desired, y_desired, z_desired, anglex, angley, anglez
-        if index>1:             ## happens way fast
-          y_desired[3]=-den1[1]*y_desired[2]-den1[2]*y_desired[1]+num1[1]*ay_raw_small[2]+num1[2]*ay_raw_small[1]
-          x_desired[3]=-den1[1]*x_desired[2]-den1[2]*x_desired[1]+num1[1]*ax_raw_small[2]+num1[2]*ax_raw_small[1]
-          ax_tilt[3]=-den2a[1]*ax_tilt[2]-den2a[2]*ax_tilt[1]+num2a[1]*ay_raw_small[2]+num2a[2]*ay_raw_small[1]
-          if abs(ax_tilt[3])>1.0:
-            ax_tilt[3] = sign(ax_tilt[3])
-          ax_tiltLP=math.asin(ax_tilt[3])
-          ay_tilt[3]=-den2[1]*ay_tilt[2]-den2[2]*ay_tilt[1]+num2[1]*ax_raw_small[2]+num2[2]*ax_raw_small[1]
-          if abs(ay_tilt[3])>1.0:
-            ay_tilt[3] = sign(ax_tilt[3])
-          ay_tiltLP=math.asin(-ay_tilt[3])
-
-          anglex_filtered[3]=-den3[1]*anglex_filtered[2]-den3[2]*anglex_filtered[1]+num3[1]*anglex_raw_small[2]+num3[2]*anglex_raw_small[1]
-          angley_filtered[3]=-den3[1]*angley_filtered[2]-den3[2]*angley_filtered[1]+num3[1]*angley_raw_small[2]+num3[2]*angley_raw_small[1]
-          anglex=ax_tiltLP+anglex_filtered[3]
-          angley=ay_tiltLP+angley_filtered[3]
-
-          z_desired[3]=-den4[1]*z_desired[2]-den4[2]*z_desired[1]-den4[3]*z_desired[0]+num4[1]*az_raw_small[2]+num4[2]*az_raw_small[1]+num4[3]*az_raw_small[0]
-          anglez_filtered[3]=-den5[1]*anglez_filtered[2]-den5[2]*anglez_filtered[1]+num5[0]*anglezraw+num5[1]*anglez_raw_small[2]+num5[2]*anglez_raw_small[1]
-        else:
-          y_desired[3]=0
-          x_desired[3]=0
-          z_desired[3]=0
-          ay_tilt[3]=0 
-          ax_tilt[3]=0 
-          anglex=0
-          angley=0
-          anglex_filtered[3]=0
-          angley_filtered[3]=0
-          anglez_filtered[3]=anglezraw
-        #how many times have we tried to filteR?
-        index=index+1
-        #what is our command?
-
-        ##original
-        #command = [x_desired[-1],y_desired[-1],z_desired[-1],ax_tiltLP,ay_tiltLP,anglez_filtered[-1]]
-
-        ##attempt
-        
-        #set a soft limit for the angle commands...
-        angle_softlim = 0.1
-
-        xdes_final = 0#x_desired[-1]/100.0
-        ydes_final = 0#y_desired[-1]/100.0
-        zdes_final = 0#z_desired[-1]/100.0
-        rdes_final = ax_tiltLP
-        if abs(rdes_final)>angle_softlim:
-          rdes_final = sign(rdes_final)*angle_softlim
-        pdes_final = ay_tiltLP
-        if abs(pdes_final)>angle_softlim:
-          pdes_final = sign(pdes_final)*angle_softlim
-        ades_final = anglez_filtered[-1]
-        if abs(ades_final)>angle_softlim:
-          ades_final = sign(ades_final)*angle_softlim
-        #rdes_final = 0
-        ades_final = 0 #TODO RELEASE THIS CONSTRAINT WHEN COMFORTABLE.
-        command = [xdes_final,ydes_final,zdes_final,rdes_final,pdes_final,ades_final]
-
-        #this worked before, but we just did some stuff above.
-        #command = [x_desired[-1]/100.0,y_desired[-1]/100.0,(z_desired[-1]/100.0)+.5,ax_tiltLP,ay_tiltLP,anglez_filtered[-1]]
-
-        # commandtime = time.time()
-        # tcomm = commandtime - timetime
-        # print tcomm
-      #print "not frozen"
-      if tnow-lastsendtime>arduino_delay:     ### also happens super fast
-        #print 'sent'
-        #print "sent: "+format(command[0],'0.2f')+","+format(command[1],'0.2f')+","+format(command[2],'0.2f')+","+format(command[3],'0.4f')+","+format(command[4],'0.4f')+","+format(command[5],'0.4f')
-        lastsendtime = tnow
-        ser.write('!')
-        for ind in range(0,len(command)-1):
-          ser.write(format(command[ind],'0.2f'))
-          ser.write(',')
-        ser.write(str(command[-1]))
-        ser.write('\n')
-        #line = ser.readline()
-        #print "recieved: "+line
-    else:
-      time.sleep(.1)
-        
-        # lasttime=time.time()
-        # endend=lasttime-timetime
-        # print endend
-
-
-
-
-
-####### execute the thread
+####### this bottom "if" only runs if you run this file as a script, like $python washout.py
+#it acts as a "demo" for the library.
 if __name__ == "__main__" :
+
+  #this demo simulates platform motion for a step in acceleration and yaw rate
+  g = 9.81#m/s/s, gravitational constant
+  vehicle_speed = 20.0 #m/s, forward speed
+  vehicle_accel = 9.81 #m/s/s, lateral acceleration
+  #use max acceleration and fwd speed to get a turn radius
+  #U^2/R = ay soo.... U^2/ay = R
+  turn_radius = vehicle_speed**2/vehicleaccel
+  #yaw rate can then be computed as U/R (just geometry...)
+  yawrate = vehicle_speed/turn_radius
+
+  #now create fake raw inputs for each of the vehicle's IMU readings that are read by washout:
+  #decide what timestep we'll run at (when running in "real time" this will depend on your threads and time.sleep())
+  dt = 0.01
+  #set up an array of time values to loop through
+  tsim = arange(0,4,dt)
+
+  #set up vectors for each variable
+  ax = zeros(size(tsim))
+  ay = ones(size(tsim))*vehicle_accel #at t=0, vehicle enters turn "perfectly" (no dynamics)
+  az = zeros(size(tsim))
+  wz = ones(size(tsim))*yawrate #at t=0, vehicle enters turn "perfectly" (no dynamics)
+
+  #now create the washout object
+  wash = Washout(dt)#be sure to specify the CORRECT dt to the best of your ability!
+
+  #create a matrix to hold our platform commands x,y,z,roll,pitch,yaw
+  cmdvec = zeros(len(t),6)#each row will represent commands at a particular timestep
+
+  #now set up a for-loop to simulate this happening.
+  #in "live" setting, this is a "while 1" infinite loop, and you're just calling the washout each time.
+
+  for k in range(0,len(tsim)):
+    #IRL you'd measure ax,ay,az,wz from a simulation, then to this to get commands.
+    #we are pulling commands out of a vector because we've pre-stored them... but you would just use like ax/g...
+    x,y,z,roll,pitch,yaw = wash.doWashout(ax[k]/g,ay[k]/g,az[k]/g,wz[k])
+    #now for plotting, store everything in our array of commands
+    cmdvec[k,:] = [x,y,z,roll,pitch,yaw]
+
+  #now we've gotten our results, so let's plot
+  figure()
+  subplot
 
     
